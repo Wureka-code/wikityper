1727b8d4afa3a4619e7e2c98ab15f8696fd06566
Wureka-code <106289422+Wureka-code@users.noreply.github.com>
2022-06-05 18:32:03 +1000

    quick commit before dinner

--- main.py
+++ main.py
@@ -4,9 +4,11 @@ from tkinter import TOP, ttk
 class mainapp(ttk.Frame):
     def __init__(self, parent, *args, **kwargs):
         ttk.Frame.__init__(self, parent, *args, **kwargs)
-        self.title = ttk.Label(text='Wikityper', font=('Helvetica', 26))
+        self.title = ttk.Label(text='Wikityper', font=('Helvetica', 54))
         self.title.pack(side=TOP)
 
 if __name__ == '__main__':
     root = tk.Tk()
+    root.geometry('1920x1080')
     app = mainapp(root)
+    app.mainloop()
