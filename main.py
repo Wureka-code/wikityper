00de85f0c4ead944284d905dd0d0ab4ce35bcfb5
Wureka-code <106289422+Wureka-code@users.noreply.github.com>
2022-06-06 18:55:28 +1000

    V1.0!!!!

--- main.py
+++ main.py
@@ -1,11 +1,14 @@
 import tkinter as tk
 from tkinter import TOP, ttk
 import wikiget
+import time
 
 class mainapp(ttk.Frame):
     def __init__(self, parent, *args, **kwargs):
         ttk.Frame.__init__(self, parent, *args, **kwargs)
         
+        self.passage = ''
+
         self.title = ttk.Label(text='Wikityper', font=('Helvetica', 54))
         self.title.pack(side=TOP)
         
@@ -15,13 +18,38 @@ class mainapp(ttk.Frame):
 
         self.startbutton = ttk.Button(command=lambda : self.startgame(), text='start')
         self.startbutton.place(relx=.5, rely=.3, anchor='c')
+
+        
     
     def startgame(self):
-        passage = wikiget.getsum()
-        self.passagelabel = tk.Message(text=passage, font=('helvetica', 24), width=800)
+        # define variables 
+        self.entrypassage = None # used to get entrypassage 
+        self.passage = wikiget.getsum()
+        self.passagelabel = tk.Message(text=self.passage, font=('helvetica', 24), width=800)
         self.passagelabel.place(relx=.5, rely=.3, anchor='c')
         self.startbutton.destroy()
+        self.starttime = time.time()
         
+        def update(): #updates every 1/10th of a second and checks if the text is finished. if it is finished, displays results.
+            self.entrypassage = self.entrybar.get() 
+            print(self.entrypassage)
+            if self.entrypassage == self.passage:
+                self.endtime = time.time()
+                self.finishtime = round(self.endtime - self.starttime, 2)
+                self.kps = round(len(self.passage) / self.finishtime, 2)
+                
+                self.timelabel = ttk.Label(text=f'Congratulations! you completed this text in {self.finishtime} seconds. your KPS was {self.kps}')
+                
+                self.passagelabel.destroy()
+                self.timelabel.place(relx=.5, rely=.3, anchor="c")
+            else:
+                self.after(100, update)
+        self.after(100, update)
+    
+        
+        
+        
+       
 
 if __name__ == '__main__':
     root = tk.Tk()
